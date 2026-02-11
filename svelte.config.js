c1b6f6b56881cd52f674119f1821318b71c9478e
Codanaut <cody@codanaut.com>
2026-02-11 15:08:54 -0700

    fixed deploy issues

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			// Changed from index.html to 404.html to better handle 
-			// GitHub Pages' specific routing behavior
-			fallback: '404.html'
+			fallback: 'index.html' // Tauri and Docker prefer index.html over 404.html
 		}),
 		paths: {
-			// This reads the BASE_PATH set in your GitHub Action
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			// Only apply a base path if we are specifically building for GH Pages
+			base: process.env.NODE_ENV === 'production' && process.env.BASE_PATH ? process.env.BASE_PATH : '',
+			relative: true // This is the key for Tauri/EXE compatibility
 		}
 	}
 };
