356ece22592d23c86189506b7d0f08e66fd34697
Codanaut <cody@codanaut.com>
2026-02-13 16:42:31 -0700

    mobile - features only show 4 albums

--- src/lib/components/layouts/StandardGrid.svelte
+++ src/lib/components/layouts/StandardGrid.svelte
@@ -1,20 +1,23 @@
 <script>
     import AlbumCard from "../AlbumCard.svelte";
     import ArtistCard from "../ArtistCard.svelte";
+    import { isMobileDevice } from "$lib/utils/deviceUtils";
 
     /** @type {any[]} */
     export let items = [];
     /** @type {'album' | 'artist'} */
     export let type = "album";
+
+    $: displayItems = isMobileDevice() ? items.slice(0, 4) : items;
 </script>
 
 <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-4">
     {#if type === "album"}
-        {#each items as item (item.id)}
+        {#each displayItems as item (item.id)}
             <AlbumCard album={item} />
         {/each}
     {:else if type === "artist"}
-        {#each items as item (item.id)}
+        {#each displayItems as item (item.id)}
             <ArtistCard artist={item} />
         {/each}
     {/if}
